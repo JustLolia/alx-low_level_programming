f3ee96ef31589895e9dd0613e2530e6c8d3786c0
JustLolia <lolialuke54@gmail.com>
2023-08-10 17:57:36 -0700

    4-put.c

--- 0x00-hello_world/4-put.c
+++ 0x00-hello_world/4-put.c
@@ -1,10 +1,11 @@
-#include <studio.h>
+#include <stdio>
 /**
-*main - A program that prints a line with put function
-*Return: 0(success)
-*/
+ * main - A program that prints a line with put function
+ *
+ * Return: 0 (success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
