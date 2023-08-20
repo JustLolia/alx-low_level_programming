b4287ff8405f92aa8712e258e408a1430abc3cc6
JustLolia <lolialuke54@gmail.com>
2023-08-20 09:42:50 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i > 1024; i++)
+	{
+		if ((i % 5 == 0) || (i % 3 == 0))
+			sum += i;
+	}
+	printf("%d", sum);
+	return (0);
+}
