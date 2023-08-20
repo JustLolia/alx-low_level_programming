31d589fe69a70decd771689c4226f16ee87d0270
JustLolia <lolialuke54@gmail.com>
2023-08-20 03:27:27 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_last_digit - get the last digit of a number
+ *
+ * @r: an integer input
+ *
+ * Description: print with _putchar declared
+ *
+ * Return: lasr digit of r
+ */
+int print_last_digit(int r)
+{
+	int i;
+
+	if (r < 0)
+	{
+		i = -1 * (r % 10);
+	}
+	else
+	{
+		i = r % 10;
+	}
+	_putchar((i % 10) + '0');
+	return (i % 10);
+}
