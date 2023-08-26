0a8f2371485351521515189e82e345b53622ead8
JustLolia <lolialuke54@gmail.com>
2023-08-26 02:59:35 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: The string to print
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int j = 0;
+	int c;
+
+	while (str[j] != '\0')
+	{
+	j++;
+	}
+	if (j % 2 == 1)
+	{
+	c = (j - 1) / 2;
+	c += 1;
+	}
+	else
+	{
+	c = j / 2;
+	}
+
+	for (; c < j; c++)
+	{
+	_putchar(str[c]);
+	}
+	_putchar('\n');
+}
