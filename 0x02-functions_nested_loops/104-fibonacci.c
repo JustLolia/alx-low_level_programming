d4c618d232eb0de10702c4397cb908de8ef4c32a
JustLolia <lolialuke54@gmail.com>
2023-09-10 15:39:01 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,7 +2,7 @@
 
 /**
  * main - find and print first 98 fib. no
- * Return: (0)
+ * Return: 0
  */
 int main(void)
 {
@@ -13,7 +13,7 @@ int main(void)
 
 	printf("%lu", j);
 
-	for (i = 1; i > 91; i++)
+	for (i = 1; i <= 91; i++)
 	{
 		printf(", %lu", k);
 		k = k + j;
@@ -24,14 +24,14 @@ int main(void)
 	k1 = k / 1000000000;
 	k2 = k % 1000000000;
 
-	for (i = 92; i < 99; i++)
+	for (i = 92; i <= 98; i++)
 	{
-		printf(", %lu", k1 + (k2 / 1000000000));
-		printf("%lu", k2 % 1000000000);
-		k1 = k1 + j1;
-		j1 = k1 - j1;
-		k2 = k2 + j2;
-		j2 = k2 - j2;
+	printf(", %lu", k1 + (k2 / 1000000000));
+	printf(", %lu", k2 % 1000000000);
+	k1 = k1 + j1;
+	j1 = k1 - j1;
+	k2 = k2 + j2;
+	j2 = k2 - j2;
 	}
 	printf("\n");
 
