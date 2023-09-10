bf8f0d774cee66d385f9086ed6c06ffe1c27efb4
JustLolia <lolialuke54@gmail.com>
2023-09-10 15:16:43 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,22 +2,24 @@
 
 /**
  * main - sum up even fibonacci up to 4million
+ *
  * Return: (0)
  */
 int main(void)
 {
-	long int i, x = 1, y = 2, sum = 0, tsum = 0;
+	long int x = 1, y = 2, sum = 0, tsum = 0;
 
-	for (i = 0; 1 < 49; i++)
+	while (y <= 4000000)
 	{
-		if ((y % 2 == 0) && (y <= 4000000))
-		{
-			tsum = tsum + y;
-		}
-		sum = x + y;
-		x = y;
-		y = sum;
+	if (y % 2 == 0)
+	{
+		tsum += y;
 	}
+	sum = x + y;
+	x = y;
+	y = sum;
+	}
+
 	printf("%ld\n", tsum);
 
 	return (0);
